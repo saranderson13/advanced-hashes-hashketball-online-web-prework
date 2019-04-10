da071f5451632301b7886a380db8f97c1d3f1ca5
Sarah Anderson <saranderson13@gmail.com>
2019-04-10 23:02:52 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -147,7 +147,10 @@ end
 def team_names
   # returns an array with both team names
   teams = []
-  game_hash.each { |home_away, team_info| teams << team_info[:team_name] }
+  game_hash.each do |home_away, team_info| 
+    binding.pry
+    teams << team_info[:team_name] 
+  end
   teams
 end
 
