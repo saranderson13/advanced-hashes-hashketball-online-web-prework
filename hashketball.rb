4f3c4f500209e71dd9d64d3e2e276e8600826269
Sarah Anderson <saranderson13@gmail.com>
2019-04-10 22:55:31 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -123,14 +123,14 @@ end
 def num_points_scored(player_name)
   # returns the number of points scored by a player
   
-  player_points = 0
+  # player_points = 0
 
   game_hash.each do |home_away, team_info|
     team_info[:players].each do |player, stats| 
-      player_points = stats[:points] if player == player_name 
+      return stats[:points] if player == player_name 
     end
   end
-  player_points
+  # player_points
 end
 
 
